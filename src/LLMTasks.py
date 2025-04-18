e810120aa7b1f7edb21d4a4fc5d98f5e7c37c559
appbird <project.appbird@gmail.com>
2025-04-18 20:35:35 +0900

    関数の構造を整理

--- src/LLMTasks.py
+++ src/LLMTasks.py
@@ -11,30 +11,31 @@ def load_prompt(path:Path) -> str:
     """
     with open(path, "r") as f: return f.read()
 
-def ask(instruction:str, input:str) -> str:
+def ask(instruction:str, user_prompt:str, model_name:str) -> str:
     """
     ask LLM to process `input` with `instruction`.
     """
     response = openai.chat.completions.create(
-        model="o4-mini-2025-04-16",
+        model=model_name,
         messages=[
             {"role": "developer", "content": instruction},
-            {"role": "user", "content": input}
+            {"role": "user", "content": user_prompt}
         ]
     )
     res = response.choices[0].message.content
     assert res
     return res
 
-def ask_with_path(instr_path:Path, input:str) -> str:
+def ask_with_path(instr_path:Path, user_prompt:str, model_name:str) -> str:
     """
     ask LLM to process `input` with the prompt in `instr_path`.
     """
     developer_prompt = load_prompt(instr_path)
-    return ask(developer_prompt, input)
+    return ask(developer_prompt, user_prompt, model_name)
 
 class LLMTasks:
     paths: list[Path]
+    model_name: str
     def __init__(
         self,
         path_or_paths: Union[str, list[Path]]
@@ -50,11 +51,11 @@ class LLMTasks:
     def __or__(self, other: LLMTasks) -> LLMTasks:
         return LLMTasks(self.paths + other.paths)
 
-    def execute(self, text: str) -> str:
+    def execute(self, text: str, model_name:str) -> str:
         """
         Execute the pipeline: sequentially call ask_with_path for each stored path.
         """
         result: str = text
         for path in self.paths:
-            result = ask_with_path(path, result)
+            result = ask_with_path(path, result, model_name)
         return result
--- src/getEDA.py
+++ src/getEDA.py
@@ -1,44 +1,67 @@
+from datetime import date, datetime
+from pathlib import Path
 import openai
 import os
 from LLMTasks import LLMTasks
 from loader import load_cached_texts
+from utility import extract_last_code_block
 
-P = "secrets/.OPENAI_API_KEY"
-with open(P, "r") as f:
-    openai.api_key = f.read().strip()
 
-def judgeEDA(): return LLMTasks("./src/prompts/judge-EDA.txt") | LLMTasks("./src/prompts/translate.txt")
+EXE_DATETIME = datetime.now()
+TESTED_FOLDER = ["test"] # "noEDA","littleEDA",
+MODEL_NAME = "o4-mini-2025-04-16"
+RESULT_DIR = "result/o4-mini"
+REPEAT_TIME = 1
 
-def getEDA(text: str, log_path: str, path: str):
-    # タスクをパイプで繋げられる
-    translated  = judgeEDA().execute(text)
-    filename = os.path.basename(path)
+def judgeEDA():     return LLMTasks("./src/prompts/judge-EDA.txt") | LLMTasks("./src/prompts/translate.txt")
+def extractEDA():   return LLMTasks("./src/prompts/extract-EDA.txt")
+TASKS = judgeEDA()
 
+def main():
+    # openaiのキーを設定しておく
+    P = "secrets/.OPENAI_API_KEY"
+    with open(P, "r") as f:
+        openai.api_key = f.read().strip()
+    # 実験する
+    result_folder = Path(RESULT_DIR)/EXE_DATETIME.strftime("%Y%m%d_%H%M%S")
+    os.makedirs(result_folder,exist_ok=True)
+    for fl in TESTED_FOLDER:
+        cache_dir = f"PDF/cache/{fl}"
+        texts, paths = load_cached_texts(cache_dir)
+        # 各テキストに対して EDA 推定を実行
+        for i in range(REPEAT_TIME):
+            log_file_path = result_folder/f"{fl}{i}.json"
+            read_folder_and_report(texts, paths, f"{log_file_path}")
+
+def read_folder_and_report(
+        papers:list[str],
+        paths:list[str],
+        result_file_path:str,
+        max_limit_files:int = 99
+    ):
+    """
+    paths[i]に格納されている論文papers[i]を読み、求めたタスクに対する結果を
+    """
+    readed_count = 0
+    max_files = min(max_limit_files,len(paths))
+    for text, path in zip(papers, paths):
+        print(f"===={readed_count+1}/{max_files} {os.path.basename(path)} ====")
+        translated = judgeEDA().execute(text, MODEL_NAME)
+        report_result(translated, result_file_path)
+        readed_count += 1
+        if readed_count >= max_files: break
+
+def report_result(
+        answer_from_LLM: str, json_file_path: str,
+    ):    
+    json_data = extract_last_code_block(answer_from_LLM)
     # ログ出力
-    with open(log_path, "a", encoding="utf-8") as logf:
-        logf.write(f"==== {filename} ====\n")
-        logf.write(translated + "\n\n")
+    with open(json_file_path, "a", encoding="utf-8") as logf:
+        logf.write(json_data)
     # 標準出力
-    print(f"===={readed+1}/{max_files} {filename} ====")
-    print(translated)
+    print(json_data)
     print("\n")
 
 
-# キャッシュされたテキストを読み込む
-files = ["test"] # "noEDA","littleEDA",
-target_dir = "result/"
-os.makedirs(target_dir,exist_ok=True)
-
-for fl in files:
-    cache_dir = f"PDF/cache/{fl}"  # ここを実際のディレクトリに置き換えて
-    texts, paths = load_cached_texts(cache_dir)
-    # 各テキストに対して EDA 推定を実行
-    for i in range(5):
-        log_file_path = f"{target_dir}{fl}{i+1}.txt"
-        max_files = min(99,len(paths))
-        readed = 0
-        for text, path in zip(texts, paths):
-            getEDA(text, log_file_path, path)
-            readed += 1
-            if readed >= max_files:
-                break
+if __name__ == "__main__":
+    main()
