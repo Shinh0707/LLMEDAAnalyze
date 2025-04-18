7a9fc108b41d8337b5477ac3408a88d76755e341
appbird <project.appbird@gmail.com>
2025-04-19 00:35:16 +0900

    bugfix

--- src/LLMTasks.py
+++ src/LLMTasks.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from pathlib import Path
 from pathlib import Path
-from typing import Sequence, Union
+from typing import Union
 
 import openai
 
--- src/getEDA.py
+++ src/getEDA.py
@@ -8,14 +8,14 @@ from utility import extract_last_code_block
 
 
 EXE_DATETIME = datetime.now()
-TESTED_FOLDER = ["test"] # "noEDA","littleEDA",
+TESTED_FOLDER = ["hasEDA"] # "noEDA","littleEDA",
 MODEL_NAME = "o4-mini-2025-04-16"
-RESULT_DIR = "result/o4-mini"
+RESULT_DIR = "result/o4-mini-extractEDA"
 REPEAT_TIME = 1
 
 def judgeEDA():     return LLMTasks("./src/prompts/judge-EDA.txt") | LLMTasks("./src/prompts/translate.txt")
 def extractEDA():   return LLMTasks("./src/prompts/extract-EDA.txt")
-TASKS = judgeEDA()
+TASKS = extractEDA()
 
 def main():
     # openaiのキーを設定しておく
@@ -23,40 +23,41 @@ def main():
     with open(P, "r") as f:
         openai.api_key = f.read().strip()
     # 実験する
-    result_folder = Path(RESULT_DIR)/EXE_DATETIME.strftime("%Y%m%d_%H%M%S")
-    os.makedirs(result_folder,exist_ok=True)
+    result_folder = Path(RESULT_DIR)/(EXE_DATETIME.strftime("%Y%m%d_%H%M%S"))
     for fl in TESTED_FOLDER:
         cache_dir = f"PDF/cache/{fl}"
         texts, paths = load_cached_texts(cache_dir)
         # 各テキストに対して EDA 推定を実行
         for i in range(REPEAT_TIME):
-            log_file_path = result_folder/f"{fl}{i}.json"
-            read_folder_and_report(texts, paths, f"{log_file_path}")
+            result_json_folder = result_folder/f"{fl}{i}"
+            os.makedirs(result_json_folder,exist_ok=True)
+            read_folder_and_report(texts, paths, result_json_folder)
 
 def read_folder_and_report(
         papers:list[str],
         paths:list[str],
-        result_file_path:str,
+        result_json_folder:Path,
         max_limit_files:int = 99
     ):
     """
-    paths[i]に格納されている論文papers[i]を読み、求めたタスクに対する結果を
+    paths[i]に格納されている論文papers[i]を読み、
+    求めたタスクに対する結果をJSONファイルとして出力する。
     """
     readed_count = 0
     max_files = min(max_limit_files,len(paths))
     for text, path in zip(papers, paths):
         print(f"===={readed_count+1}/{max_files} {os.path.basename(path)} ====")
-        translated = judgeEDA().execute(text, MODEL_NAME)
-        report_result(translated, result_file_path)
+        translated = TASKS.execute(text, MODEL_NAME)
+        report_result(translated, result_json_folder/f"{Path(path).name}.json")
         readed_count += 1
         if readed_count >= max_files: break
 
 def report_result(
-        answer_from_LLM: str, json_file_path: str,
+        answer_from_LLM: str, json_file_path: Path,
     ):    
     json_data = extract_last_code_block(answer_from_LLM)
     # ログ出力
-    with open(json_file_path, "a", encoding="utf-8") as logf:
+    with open(json_file_path, "w", encoding="utf-8") as logf:
         logf.write(json_data)
     # 標準出力
     print(json_data)
