3494fee93ccef1cfd781a3f086a4f8ba4de0f16c
appbird <project.appbird@gmail.com>
2025-04-25 22:08:46 +0900

    3072次元に戻した

--- src/embedding.py
+++ src/embedding.py
@@ -17,7 +17,6 @@ def get_embedding(text: str, model: str = "text-embedding-3-large"):
     response = client.embeddings.create(
         input=text,
         model=model,
-        dimensions=2,
         encoding_format="float"
     )
     return np.array(response.data[0].embedding)
