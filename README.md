40f676a2535f0e2e3fd1a6021468ed040a340406
Appbird <project.appbird@gmail.com>
2025-05-02 17:22:48 +0900

    EC202*のPDFをすべて変換

---
+++ README.md
@@ -0,0 +1,4 @@
+# LLMEDAAnalysis
+## 実行における前提条件
+- [Box](https://kwansei.box.com/s/mc17755i047wte124crvg6yjty8y4fvq)からPDFをDLして、`./PDF`に格納する
+- `src/loadPDF.py`を実行する。
--- src/loadPDF.py
+++ src/loadPDF.py
@@ -1,6 +1,7 @@
 import pymupdf4llm
 import os
 import glob
+from tqdm import tqdm
 
 def load(file: str):
     return pymupdf4llm.to_markdown(file)
@@ -11,7 +12,7 @@ def load_all(dir: str, cache_dir: str):
     texts = []
     pdf_files = glob.glob(os.path.join(dir, "*.pdf"))
     
-    for pdf_file in pdf_files:
+    for pdf_file in tqdm(pdf_files, desc=dir):
         filename = os.path.splitext(os.path.basename(pdf_file))[0] + ".txt"
         cache_path = os.path.join(cache_dir, filename)
         if os.path.exists(cache_path):
@@ -28,15 +29,15 @@ def load_all(dir: str, cache_dir: str):
 if __name__ == "__main__":
     pdf_dir = "PDF"
     cache_dir = "PDF/cache"
-    has_EDAdir = os.path.join(pdf_dir,"hasEDA")
-    has_EDA_cachedir = os.path.join(cache_dir,"hasEDA")
-    little_EDAdir = os.path.join(pdf_dir,"littleEDA")
-    little_EDA_cachedir = os.path.join(cache_dir,"littleEDA")
-    not_EDAdir = os.path.join(pdf_dir,"notEDA")
-    not_EDA_cachedir = os.path.join(cache_dir,"notEDA")
-    test_dir = os.path.join(pdf_dir,"test")
-    test_cachedir = os.path.join(cache_dir,"test")
-    load_all(has_EDAdir, has_EDA_cachedir)
-    load_all(not_EDAdir, not_EDA_cachedir)
-    load_all(little_EDAdir, little_EDA_cachedir)
-    load_all(test_dir, test_cachedir)
+
+    def EDA_load_all(folder_name:str):
+        directory = os.path.join(pdf_dir,folder_name)   
+        cachedir = os.path.join(cache_dir,folder_name)
+        load_all(directory, cachedir) 
+    
+    EDA_load_all("hasEDA")
+    EDA_load_all("littleEDA")
+    EDA_load_all("notEDA")
+    EDA_load_all("ECSympo/EC2022")
+    EDA_load_all("ECSympo/EC2023")
+    EDA_load_all("ECSympo/EC2024")
